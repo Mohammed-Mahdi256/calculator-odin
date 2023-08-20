ebdf3494767c64733c9a1abe49d3a089f6606d60
Mohammed-Mahdi256 <mohammedmahdi1402@gmail.com>
2023-08-20 09:50:47 +0200

    Created boilerplate and the four basic function addition substraction multiplication and division (returns undefined with second param equal to 0)

---
+++ index.js
@@ -0,0 +1,12 @@
+function add(a, b){
+    return a + b;
+}
+function subs(a, b){
+    return a - b;
+}
+function multiply(a, b){
+    return
+}
+function devide(a, b){
+    b !== 0 ? a/b : undefined;
+}
