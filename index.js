835f654959b612b1b6260b7a9a66923b433921f5
Mohammed-Mahdi256 <mohammedmahdi1402@gmail.com>
2023-08-21 17:01:49 +0200

    Created the UI, js not wired yet, random colors

--- index.js
+++ index.js
@@ -5,8 +5,45 @@ function subs(a, b){
     return a - b;
 }
 function multiply(a, b){
-    return
+    return a * b;
 }
 function devide(a, b){
-    b !== 0 ? a/b : undefined;
+    return b === 0 ? undefined : (a/b).toFixed(2);
 }
+
+function operate(a, b, o) {
+    switch (o) {
+        case '+':
+            return add(a, b);
+            break;
+        case '-':
+            return subs(a, b);
+            break;
+        case '*':
+            return multiply(a, b);
+            break;
+        case '/':
+            return devide(a, b);
+
+        default:
+            break;
+    }
+}
+
+
+
+let operation = '';
+let firstNum = 4;
+let secondNum = 66;
+
+operation = '-'
+console.log(operate(firstNum, secondNum, operation));
+operation = '+'
+console.log(operate(firstNum, secondNum, operation));
+operation = '*'
+console.log(operate(firstNum, secondNum, operation));
+operation = '/'
+console.log(operate(firstNum, secondNum, operation));
+secondNum = 0;
+operation = '/'
+console.log(operate(firstNum, secondNum, operation));
