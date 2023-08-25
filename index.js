803e71e6e9b0e4b63ae80789666598e451642ebe
Mohammed-Mahdi256 <mohammedmahdi1402@gmail.com>
2023-08-25 16:25:30 +0200

    Done, it works, I'm out! the calculator works, the logic is about states of input, boolean values determine the behaviour of each button

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ function devide(a, b){
 }
 
 function operate(a, b, o) {
+    a = Number(a);
+    b = Number(b);
     switch (o) {
         case '+':
             return add(a, b);
@@ -19,13 +21,14 @@ function operate(a, b, o) {
         case '-':
             return subs(a, b);
             break;
-        case '*':
+        case 'X':
             return multiply(a, b);
             break;
         case '/':
             return devide(a, b);
 
         default:
+            return false;
             break;
     }
 }
@@ -33,17 +36,78 @@ function operate(a, b, o) {
 
 
 let operation = '';
-let firstNum = 4;
-let secondNum = 66;
+let firstNum = NaN;
+let secondNum = NaN;
+let currentDisplay = 0;
+let erase = false;
+let firstOperation = true;
+let changeSecondNum = true;
 
-operation = '-'
-console.log(operate(firstNum, secondNum, operation));
-operation = '+'
-console.log(operate(firstNum, secondNum, operation));
-operation = '*'
-console.log(operate(firstNum, secondNum, operation));
-operation = '/'
-console.log(operate(firstNum, secondNum, operation));
-secondNum = 0;
-operation = '/'
-console.log(operate(firstNum, secondNum, operation));
+
+
+
+
+const resultScreen = document.querySelector('.result-screen');
+
+const digitBtns = document.querySelectorAll('.digit');
+digitBtns.forEach(digit => {
+    digit.addEventListener('click', () => {
+        if(erase) {
+            resultScreen.textContent = '';
+            currentDisplay = '';
+            erase = false;
+        }
+        
+
+        
+        if(resultScreen.textContent == '0') currentDisplay = digit.textContent;
+        else currentDisplay = resultScreen.textContent + digit.textContent;
+
+        resultScreen.textContent = currentDisplay;
+        changeSecondNum = true;
+        
+    })
+})
+
+const operationBtns = document.querySelectorAll('.operation');
+operationBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        if(! firstOperation){
+            secondNum = currentDisplay;
+            
+            resultScreen.textContent = operate(firstNum, secondNum, operation);
+            currentDisplay = resultScreen.textContent;
+        }
+        operation = btn.textContent;
+        firstNum = currentDisplay;
+        erase = true;
+        firstOperation = false;
+        changeSecondNum = true;
+        
+            
+    });
+});
+
+const equalBtn = document.querySelector('.equal-btn');
+equalBtn.addEventListener('click', () => {
+    if(! firstOperation){
+        if(changeSecondNum) {
+            secondNum = currentDisplay;
+            changeSecondNum = false;
+        }
+        if(secondNum == 0 && operation == '/') {
+            clear();
+            return;
+        }
+        resultScreen.textContent = operate(firstNum, secondNum, operation);
+        currentDisplay = resultScreen.textContent;
+    }
+    firstNum = currentDisplay;
+    erase = true; 
+    firstOperation = true;
+});
+
+
+function clear(){
+
+}
